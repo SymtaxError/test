6d434f1f061529f47e0a101c0a78a243068d3340
rostislav <kluskape@gmail.com>
2022-09-24 18:24:45 +0300

    init

---
+++ build.gradle.kts
@@ -0,0 +1,32 @@
+val ktor_version: String by project
+val kotlin_version: String by project
+val logback_version: String by project
+
+plugins {
+    application
+    kotlin("jvm") version "1.7.10"
+    id("io.ktor.plugin") version "2.1.1"
+}
+
+group = "symtaxerror.symcha.test"
+version = "0.0.1"
+application {
+    mainClass.set("symtaxerror.symcha.test.ApplicationKt")
+
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.ktor:ktor-client-core:$ktor_version")
+    implementation("io.ktor:ktor-client-cio:$ktor_version")
+    implementation("io.ktor:ktor-client-websockets:$ktor_version")
+}
+
+tasks.named<JavaExec>("run") {
+    standardInput = System.`in`
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "symcha-websocket-test"
